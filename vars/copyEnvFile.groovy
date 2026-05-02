e1bc151ac47c8014937e075029de5c428359617e
Denis <asrock2@asrock.localdomain>
2026-05-02 18:04:28 +0500

    Use color func in copyEnvFile func

--- vars/copyEnvFile.groovy
+++ vars/copyEnvFile.groovy
@@ -1,7 +1,10 @@
 def call(String credentialsId, String variableName, String pathFromCopy) {
-    echo "--- Начало копирования ENV файла ---"
-    withCredentials([file(credentialsId: credentialsId, variable: variableName)]) {
-        sh "cp ${env[variableName]} ${pathFromCopy}"
+    ansiColor('xterm') {
+        echo colors.info("--- Начало копирования ENV файла ---") \
+
+        withCredentials([file(credentialsId: credentialsId, variable: variableName)]) {
+            sh "cp ${env[variableName]} ${pathFromCopy}"
+        }
+        echo colors.success("--- Копирования ENV файла успешно завершено ---") \
     }
-    echo "--- Копирования ENV файла успешно завершено ---"
 }
