894a4adc4c92baf61d8846202691d525c3922338
Denis <asrock2@asrock.localdomain>
2026-05-02 17:49:19 +0500

    fix naming and env in cp

--- vars/copyEnvFile.groovy
+++ vars/copyEnvFile.groovy
@@ -1,7 +1,7 @@
 def call(String credentialsId, String variable_name, String path_from_copy) {
     echo "--- Начало копирования ENV файла ---"
-    withCredentials([file(credentialsId: "${credentialsId}", variable: "${variable_name}")]) {
-        sh "cp \$variable_name \$path_from_copy"
+    withCredentials([file(credentialsId: credentialsId, variable: variable_name)]) {
+        sh "cp ${env[variable_name]} ${path_from_copy}"
     }
     echo "--- Копирования ENV файла успешно завершено ---"
 }
