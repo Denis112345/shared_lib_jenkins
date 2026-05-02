2d16ce767d9c4bc6ab0efeaaaa8eeb4547619c4d
Denis <asrock2@asrock.localdomain>
2026-05-02 18:02:06 +0500

    update code style and add new func for color text print

---
+++ vars/colorsLog.groovy
@@ -0,0 +1,8 @@
+def call(String text, String colorCode = '0') {
+    return "\u001b[${code}m${text}\u001b[0m"
+}
+
+def error(String text) { call(text, '31') }
+def success(String text) { call(text, '32') }
+def warning(String text) { call(text, '33') }
+def info(String text) { call(text, '36') }
