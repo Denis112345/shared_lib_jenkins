187682db1a09462f18eec0bf44c13dfb4b463cef
Denis <asrock2@asrock.localdomain>
2026-05-02 17:43:36 +0500

    fix type name in func

--- vars/copyEnvFile.groovy
+++ vars/copyEnvFile.groovy
@@ -1,4 +1,4 @@
-def call(Streing credentialsId, String variable_name, String path_from_copy) {
+def call(String credentialsId, String variable_name, String path_from_copy) {
     echo "--- Начало копирования ENV файла ---"
     withCredentials([file(credentialsId: "${credentialsId}", variable: "${variable_name}")]) {
         sh "cp \$variable_name \$path_from_copy"
