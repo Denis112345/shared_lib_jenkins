c2e35fbaa76824c74556622d9677098faaf7aab6
Denis <asrock2@asrock.localdomain>
2026-05-03 16:11:26 +0500

    update

--- vars/copyEnvFile.groovy
+++ vars/copyEnvFile.groovy
@@ -1,14 +1,9 @@
-def call(String credentialsId, String variableName, String targetPath) {
+def call(String credentialsId, String variableName, String pathFromCopy) {
     ansiColor('xterm') {
         echo colorsLog.info("--- Начало копирования ENV файла ---")
 
         withCredentials([file(credentialsId: credentialsId, variable: variableName)]) {
-            sh """
-                ABS_PATH="\$(cd "\$(dirname "${targetPath}")" && pwd)/\$(basename "${targetPath}")"
-                cat "\$${variableName}" | tee "\${ABS_PATH}" > /dev/null
-                chmod 600 "\${ABS_PATH}"
-                echo "✅ Файл записан: \${ABS_PATH}"
-            """
+            sh "cp \"\$${variableName}\" \"${pathFromCopy}\""
         }
         echo colorsLog.success("--- Копирования ENV файла успешно завершено ---")
     }
