13da1ba1be3d3518b07bd983ba531a58e6b59850
Denis <asrock2@asrock.localdomain>
2026-05-03 15:58:24 +0500

    update

--- vars/copyEnvFile.groovy
+++ vars/copyEnvFile.groovy
@@ -3,11 +3,13 @@ def call(String credentialsId, String variableName, String pathFromCopy) {
         echo colorsLog.info("--- Начало копирования ENV файла ---")
 
         withCredentials([file(credentialsId: credentialsId, variable: variableName)]) {
-            echo "🔍 Source var name: ''' + variableName + '''"
-            echo "🔍 Source path: $''' + variableName + '''"
-            echo "🔍 Target path: ${pathFromCopy}"
-            ls -la "$''' + variableName + '''" || echo "❌ Source file not found!"
-            cp -v "$''' + variableName + '''" "${pathFromCopy}"
+            sh '''
+                echo "🔍 Source var name: ''' + variableName + '''"
+                echo "🔍 Source path: $''' + variableName + '''"
+                echo "🔍 Target path: ${pathFromCopy}"
+                ls -la "$''' + variableName + '''" || echo "❌ Source file not found!"
+                cp -v "$''' + variableName + '''" "${pathFromCopy}"
+            '''
         }
         echo colorsLog.success("--- Копирования ENV файла успешно завершено ---")
     }
